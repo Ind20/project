e1bd04830729b6f882a56b8de6b3bb0cf9b2ccfa
Ind20 <indresh.maurya@gmail.com>
2020-10-27 09:25:35 +0530

    logo image, clean form and footer css changes

--- login/forms.py
+++ login/forms.py
@@ -8,6 +8,9 @@ class contactusMessageForm(forms.ModelForm):
 
 
 class userProfileForm(forms.ModelForm):
+    profile_pic = forms.ImageField(required=False,widget=forms.FileInput)
+    age         = forms.IntegerField(required=False)
+    phone_num   = forms.CharField(max_length = 15, required=False)
     class Meta:
         model   = userProfile
         fields  = ["age", "phone_num", "profile_pic", 'user']
