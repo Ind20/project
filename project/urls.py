757ef02f6b4d0e8e09c336db59c14d3a4191ad77
Indresh Maurya <indresh.maurya@gmail.com>
2021-02-12 15:57:38 +0530

    home and details page design

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns =[
     url(r'^$', views.projects, name='products'),
-    url(r'^(?P<projectID>[a-z,0-9]+)/$', views.project_detail, name='project_detail')
+    url(r'^/[a-z,0-9]$', views.project_detail, name='project_detail')
 ]
