f32f47c02d77d7ea157e54849cd6a847f0f9dbc5
Indresh Maurya <indresh.maurya@gmail.com>
2021-02-12 17:47:34 +0530

    details page routing fixed

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from . import views
 
 urlpatterns =[
     url(r'^$', views.projects, name='products'),
-    url(r'^/[a-z,0-9]$', views.project_detail, name='project_detail')
+     path('/<int:id>', views.project_detail, name='project_detail')
 ]
