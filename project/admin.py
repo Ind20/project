e9ad2b7644d345cd7c39c24a20413483e73c2412
Indresh Maurya <indresh.maurya@gmail.com>
2021-02-12 14:05:18 +0530

    project model for admin

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import project
 
-# Register your models here.
+
+class projectAdmin(admin.ModelAdmin):
+    list_display =('Project_Category', 'Project_Name', 'Student_Name','Mentor_Name', 'Project_Image_url', 'Project_Video_url', 'Project_Description')
+
+admin.site.register(project, projectAdmin)
