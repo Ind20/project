1c968aa00ee5abc03799bbc9ca8fac97bb4ca052
Indresh Maurya <indresh.maurya@gmail.com>
2021-02-12 14:08:16 +0530

    views fix

--- project/views.py
+++ project/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from project.models import project
 
-# Create your views here.
+def projects(request):
+    projects = project.objects.all()
+    return render(request,'project/projects.html',
+                  {'projects': projects})
+
+def project_detail(request,projectID):
+    pd = project.objects.raw('SELECT * FROM project_project WHERE id =%s',[projectID])
+    for x in pd:
+        x
+    return render(request,'project/project.html',{'project':project})
