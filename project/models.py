a82543590e3f9c24c6ebce2b1881a1abe3b2db9d
Indresh Maurya <indresh.maurya@gmail.com>
2021-02-21 11:46:39 +0530

    added assets

--- project/models.py
+++ project/models.py
@@ -5,13 +5,13 @@ from django.db import models
 class projectCategory(models.Model):
     Category_Name           = models.CharField(max_length=35)
     Category_Description   = models.CharField( max_length=2000)
-    Category_Image          = models.ImageField(default='images/category/category.png', upload_to='images/category/', null='true', blank='true')
+    Category_Image          = models.ImageField(default='assets/category.png', upload_to='images/category/', null='true', blank='true')
 
 class project(models.Model):
     Project_Name            = models.CharField(max_length=350)
     Project_Category        = models.ForeignKey(projectCategory, on_delete=models.CASCADE)
     Student_Name            = models.CharField(max_length=35)
     Mentor_Name             = models.CharField(max_length=35)
-    Project_Image           = models.ImageField(default='images/project/project.png', upload_to='images/project/', null='true', blank='true')
+    Project_Image           = models.ImageField(default='assets/project.png', upload_to='images/project/', null='true', blank='true')
     Project_Video_url       = models.CharField(max_length=2085)
     Project_Description     = models.CharField( max_length=10000)
