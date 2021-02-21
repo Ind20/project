37a11bbde123939050a876b88bb56fc69b57db62
Indresh Maurya <indresh.maurya@gmail.com>
2021-02-21 17:55:37 +0530

    header and ulr changes

--- djlogin/urls.py
+++ djlogin/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects', include('project.urls')),
+    path('project', include('project.urls')),
     path('', include('djusers.urls'))
 ]
 
