d92b6554822de841e2d33d3d9ba10e0eebd223f8
Indresh Maurya <indresh.maurya@gmail.com>
2021-02-26 10:47:45 +0530

    cs

--- project/forms.py
+++ project/forms.py
@@ -18,8 +18,6 @@ class projectForm(forms.ModelForm):
 class Project_CategorySelect(forms.Select):
     def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
         option = super().create_option(name, value, label, selected, index, subindex, attrs)
-        if value:
-            option['attrs']['data-price'] = value.instance.price
         return option
 
         fields = ['Project_Category']
