c53c9831d28fd76cd86ba44d462ec0b15183144f
Indresh Maurya <indresh.maurya@gmail.com>
2021-03-12 14:07:22 +0530

    users url fix

--- djusers/urls.py
+++ djusers/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='djlogin/home'),
-    path('user/profile', views.profile, name="profile"),
-    path('user/editprofile', views.editprofile, name="editprofile"),
-    path('user/register', views.register, name='register'),
-    path('user/login', views.login, name='login'),
-    path('user/dashboard', views.dashboard, name='dashboard'),
-    path('user/logout', views.logout, name='logout'),
-    path('djlogin/header', views.header, name='header'),
-    path('djlogin/footer', views.footer, name='footer'),
-    path('djlogin/contactus', views.contactus, name='contactus')
+    path('profile', views.profile, name="profile"),
+    path('editprofile', views.editprofile, name="editprofile"),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('logout', views.logout, name='logout'),
+    path('header', views.header, name='header'),
+    path('footer', views.footer, name='footer'),
+    path('contactus', views.contactus, name='contactus')
 ]
