46e2221755e5a8c2c6408c59cdbb37902d975a71
Indresh Maurya <indresh.maurya@gmail.com>
2021-03-12 11:37:33 +0530

    category page design and projects page fixes

--- project/views.py
+++ project/views.py
@@ -8,24 +8,18 @@ def projects(request):
     projects = project.objects.all()
     return render(request,'project/projects.html', {'projects': projects})
 
-def category(request, id):
-    cat = projectCategory.objects.get(id=id)
-    context = {
-             'cat':cat,
-            }
-    return render(request, 'project/category.html', context)
+def project_detail(request, id):
+    proj = project.objects.get(id=id)
+    return render(request, 'project/project.html', {'proj': proj})
 
 def categories(request):
     categories = projectCategory.objects.all()
     return render(request,'project/categories.html', {'categories': categories})
 
-def project_detail(request, id):
-    proj = project.objects.get(id=id)
-    context = {
-             'proj':proj,
-            }
-    return render(request, 'project/project.html', context)
-
+def category(request, id):
+    projects = project.objects.filter(Project_Category_id=id)[:8]
+    category = projectCategory.objects.get(id=id)
+    return render(request, 'project/category.html', {'category':category, 'projects': projects})
 
 def addproject(request):
     form= projectForm(request.POST or None, request.FILES or None)
